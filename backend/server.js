69ba0110f56a4bee006f5dc675846e9c0b6770df
Vanithavasa <lakshmivasa888@gmail.com>
2026-02-23 20:29:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -400,7 +400,6 @@ app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 );
 
-const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
